95334ccf538111fbf05fdf896c7915dc759ffc13
Colm Moore <contact@colm.tech>
2022-06-20 15:27:18 +1000

    Fixed start:dev script Added cors Added port output on server start

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import express from 'express';
+import cors from 'cors';
 
 import staticRoute from './routes/static';
 import pdfRoute from './routes/pdf';
 
 const app: express.Application = express();
 
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/pdf', pdfRoute);
 app.use('/pdfs', staticRoute);
 
 app.listen(port, () => {
-  console.log('Now Listening...');
+  console.log(`Now Listening on Port: ${port}...`);
 });
